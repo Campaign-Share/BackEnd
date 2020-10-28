2ea4d14e54610b1798eaeb5828c945dfddcf1769
지농 <jinhong0719@naver.com>
2020-10-28 15:23:45 +0900

    [ADD] <domain/auths/UserAuth.java> Add UserAuth class to declare entity

--- src/main/java/com/cs/webservice/domain/auths/UserAuth.java
+++ src/main/java/com/cs/webservice/domain/auths/UserAuth.java
@@ -1,1 +1,14 @@
 package com.cs.webservice.domain.auths;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "user_auths")
+public class UserAuth {
+
+}
