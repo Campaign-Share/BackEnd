4b4d8fc2446793099ec8cedb994c5838baede937
지농 <jinhong0719@naver.com>
2020-10-28 15:25:14 +0900

    [ADD] <domain/auths/UserAuth.java> Add attribute & set column, constraint

--- src/main/java/com/cs/webservice/domain/auths/UserAuth.java
+++ src/main/java/com/cs/webservice/domain/auths/UserAuth.java
@@ -3,6 +3,9 @@ package com.cs.webservice.domain.auths;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -10,5 +13,19 @@ import javax.persistence.*;
 @Entity
 @Table(name = "user_auths")
 public class UserAuth {
+    @Id
+    @Column(unique = true, nullable = false, columnDefinition = "CHAR(17)", length = 17, name = "uuid")
+    @Size(min = 17, max = 17) @NotNull @NotEmpty
+    private String uuid;
 
+    @Column(unique = true, nullable = false, length = 20, name = "user_id")
+    @Size(min = 4, max = 20) @NotNull @NotEmpty
+    private String userID;
+
+    @Column(nullable = false, length = 100, name = "user_pw")
+    @NotNull
+    private String userPW;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE", name = "locked")
+    private boolean locked;
 }
