acbaaf946f7d303909cf8928dba362cbc9815168
지농 <jinhong0719@naver.com>
2020-10-28 15:46:25 +0900

    [ADD] <domain/UserAuthRepositoryTest.java> Add Test class & setting test env

--- src/test/java/com/cs/webservice/domain/UserAuthRepositoryTest.java
+++ src/test/java/com/cs/webservice/domain/UserAuthRepositoryTest.java
@@ -1,1 +1,21 @@
 package com.cs.webservice.domain;
+
+import com.cs.webservice.domain.auths.repository.UserAuthRepository;
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserAuthRepositoryTest {
+    @Autowired
+    UserAuthRepository userAuthRepository;
+
+    @After
+    public void cleanup() {
+        userAuthRepository.deleteAll();
+    }
+}
