64b0e61eef2c5e73cb1cfbc1ba7e5d362a90e9d7
지농 <jinhong0719@naver.com>
2020-10-28 15:39:31 +0900

    [ADD] <Application.java> Add @EnableJpaAuditing in Application class

--- src/main/java/com/cs/webservice/Application.java
+++ src/main/java/com/cs/webservice/Application.java
@@ -2,7 +2,9 @@ package com.cs.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
