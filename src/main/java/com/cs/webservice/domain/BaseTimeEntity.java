ca8769fff013deb2683fea2d1d067fbc2763587e
지농 <jinhong0719@naver.com>
2020-10-28 15:37:55 +0900

    [ADD] <domain/BaseTimeEntity.java> Add LocalDateTime field for attribute

--- src/main/java/com/cs/webservice/domain/BaseTimeEntity.java
+++ src/main/java/com/cs/webservice/domain/BaseTimeEntity.java
@@ -1,6 +1,9 @@
 package com.cs.webservice.domain;
 
 import lombok.Getter;
+import org.joda.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
@@ -10,5 +13,9 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
 
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
