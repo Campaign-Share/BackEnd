66bb757aa069c78f066c0f9971e1928553b90ebe
지농 <jinhong0719@naver.com>
2020-10-28 17:08:08 +0900

    [CREATE] <controller/auths/AuthController.java> Add AuthController & /v1/users api

--- src/main/java/com/cs/webservice/controller/auths/AuthController.java
+++ src/main/java/com/cs/webservice/controller/auths/AuthController.java
@@ -1,2 +1,23 @@
-package com.cs.webservice.controller.auths;public class AuthController {
+package com.cs.webservice.controller.auths;
+
+import com.cs.webservice.domain.auths.repository.UserAuthRepository;
+import com.cs.webservice.dto.auths.CreateNewUser;
+import lombok.AllArgsConstructor;
+import org.apache.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@AllArgsConstructor
+public class AuthController {
+    private UserAuthRepository userAuthRepository;
+
+    @PostMapping(value = "/v1/users", consumes = {"application/json"})
+    public CreateNewUser.Response createNewUser(@Valid @RequestBody CreateNewUser.Request dto, BindingResult bindingResult) {
+        
+    }
 }
