eea6d769642fc4acbd858c1a9e5a826e1a9d66ba
지농 <jinhong0719@naver.com>
2020-10-28 15:31:16 +0900

    [ADD] <domain/auths/UserInform.java> Add UserInform entity class

--- src/main/java/com/cs/webservice/domain/auths/UserInform.java
+++ src/main/java/com/cs/webservice/domain/auths/UserInform.java
@@ -1,1 +1,17 @@
 package com.cs.webservice.domain.auths;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "user_informs")
+public class UserInform {
+
+}
