aa7dc6b842bfaecfa504ddda9e36cd780d7fd00b
지농 <jinhong0719@naver.com>
2020-10-28 16:05:15 +0900

    [CREATE] <dto/auths/CreateNewUser.java> Add Request &Respone static class

--- src/main/java/com/cs/webservice/dto/auths/CreateNewUser.java
+++ src/main/java/com/cs/webservice/dto/auths/CreateNewUser.java
@@ -1,1 +1,19 @@
 package com.cs.webservice.dto.auths;
+
+import lombok.*;
+
+public class CreateNewUser {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Request {
+
+    }
+
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Response {
+
+    }
+}
