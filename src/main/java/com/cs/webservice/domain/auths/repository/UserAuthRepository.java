4bd15db82cbc589f9f23d63e085b9e2d1a524d02
지농 <jinhong0719@naver.com>
2020-10-28 15:28:10 +0900

    [ADD] <domain/.../UserAuthRepository.java> Add UserAuthRepository interface

--- src/main/java/com/cs/webservice/domain/auths/repository/UserAuthRepository.java
+++ src/main/java/com/cs/webservice/domain/auths/repository/UserAuthRepository.java
@@ -1,1 +1,7 @@
 package com.cs.webservice.domain.auths.repository;
+
+import com.cs.webservice.domain.auths.UserAuth;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAuthRepository extends JpaRepository<UserAuth, String> {
+}
