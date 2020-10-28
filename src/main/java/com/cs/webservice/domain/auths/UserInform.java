03bf2dc35109ed2ef3e5c7c03316773a63c0b9a7
지농 <jinhong0719@naver.com>
2020-10-28 15:32:33 +0900

    [ADD] <domain/auths/UserInform.java> Add UserInform field for table attribute

--- src/main/java/com/cs/webservice/domain/auths/UserInform.java
+++ src/main/java/com/cs/webservice/domain/auths/UserInform.java
@@ -1,11 +1,13 @@
 package com.cs.webservice.domain.auths;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -13,5 +15,24 @@ import javax.persistence.*;
 @Entity
 @Table(name = "user_informs")
 public class UserInform {
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    @OneToOne
+    @JoinColumn(name = "user_uuid", referencedColumnName = "uuid")
+    private UserAuth userAuth;
+
+    @Column(nullable = false, length = 5, name = "name")
+    @Size(min = 2, max = 5) @NotNull @NotEmpty
+    private String name;
+
+    @Column(nullable = false, length = 10, name = "nick_name")
+    @Size(max = 10) @NotNull
+    private String nickName;
+
+    @Column(nullable = false, length = 11, name = "phone_number", columnDefinition = "CHAR(11)")
+    @Size(min = 11, max = 11) @NotNull @NotEmpty
+    @Pattern(regexp = "^010\\d{8}")
+    private String phoneNumber;
 }
