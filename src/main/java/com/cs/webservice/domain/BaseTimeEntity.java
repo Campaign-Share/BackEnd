63cebc5699fd55069e574fe106bca1e6bb02931c
지농 <jinhong0719@naver.com>
2020-10-28 15:37:22 +0900

    [ADD] <domain/BaseTimeEntity.java> Add BaseTimeEntity interface

--- src/main/java/com/cs/webservice/domain/BaseTimeEntity.java
+++ src/main/java/com/cs/webservice/domain/BaseTimeEntity.java
@@ -1,1 +1,14 @@
 package com.cs.webservice.domain;
+
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+}
