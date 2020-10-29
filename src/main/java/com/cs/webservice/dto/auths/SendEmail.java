d57b4c6a0842b2e8828ed66303a7ba92c46d1c14
지농 <jinhong0719@naver.com>
2020-10-29 13:42:53 +0900

    [ADD] <dto/auths/SendEmail.java> Add Request & Response class in SendEmail

--- src/main/java/com/cs/webservice/dto/auths/SendEmail.java
+++ src/main/java/com/cs/webservice/dto/auths/SendEmail.java
@@ -1,5 +1,26 @@
 package com.cs.webservice.dto.auths;
 
-public class SendEmail {
+import com.cs.webservice.dto.BaseResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class SendEmail {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Request {
+        @Size(max = 30) @NotNull @NotEmpty @Email
+        @JsonProperty("email")
+        private String email;
+    }
+
+    @NoArgsConstructor
+    public static class Response extends BaseResponse { }
 }
