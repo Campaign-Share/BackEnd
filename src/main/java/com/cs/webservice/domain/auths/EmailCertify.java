60d14370aab9dc7a7281bc3fc5693c61ff360ef4
지농 <jinhong0719@naver.com>
2020-10-29 10:09:15 +0900

    [ADD] <domain/auths/EmailCertify.java> Add EmailCertify entity class

--- src/main/java/com/cs/webservice/domain/auths/EmailCertify.java
+++ src/main/java/com/cs/webservice/domain/auths/EmailCertify.java
@@ -1,2 +1,18 @@
-package com.cs.webservice.domain.auths;public class EmailCertify {
+package com.cs.webservice.domain.auths;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "email_certifies")
+public class EmailCertify {
+    
 }
