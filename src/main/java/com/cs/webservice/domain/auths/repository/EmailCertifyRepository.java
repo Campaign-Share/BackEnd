23ce82e9a1fe5ff66be0caa35cb921a96a342e4c
지농 <jinhong0719@naver.com>
2020-10-29 13:15:06 +0900

    [CREATE] <domain/.../EmailCertifyRepository.java> Create EmailCerify test file

---
+++ src/main/java/com/cs/webservice/domain/auths/repository/EmailCertifyRepository.java
@@ -0,0 +1,7 @@
+package com.cs.webservice.domain.auths.repository;
+
+import com.cs.webservice.domain.auths.EmailCertify;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailCertifyRepository extends JpaRepository<EmailCertify, String> {
+}
