b4cc440d6fc4d15db2c7128001d3630e2a0edc32
지농 <jinhong0719@naver.com>
2020-10-29 10:19:38 +0900

    [ADD] <domain/auths/EmailCertify.java> Add field for attribute in EMailCertify

--- src/main/java/com/cs/webservice/domain/auths/EmailCertify.java
+++ src/main/java/com/cs/webservice/domain/auths/EmailCertify.java
@@ -5,8 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -14,5 +20,18 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "email_certifies")
 public class EmailCertify {
-    
+    @Id
+    @Column(unique = true, nullable = false, length = 30, name = "email")
+    @Size(max = 30) @NotNull @NotEmpty @Email
+    private String email;
+
+    @Column(nullable = false, length = 6, columnDefinition = "CHAR(6)", name = "auth_code")
+    @Size(min = 6, max = 6) @NotNull @NotEmpty
+    private String authCode;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE", name = "certified")
+    private boolean certified;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE", name = "using")
+    private boolean using;
 }
