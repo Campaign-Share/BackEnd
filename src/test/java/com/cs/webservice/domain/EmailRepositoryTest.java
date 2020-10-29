42a2578152a977be3b51f1dcd35278567009c027
지농 <jinhong0719@naver.com>
2020-10-29 13:35:51 +0900

    [ADD] <domain/EmailRepositoryTest.java> Add EmailCertifyAuthSaveAndLoad test

--- src/test/java/com/cs/webservice/domain/EmailRepositoryTest.java
+++ src/test/java/com/cs/webservice/domain/EmailRepositoryTest.java
@@ -1,4 +1,52 @@
 package com.cs.webservice.domain;
 
+import com.cs.webservice.domain.auths.EmailCertify;
+import com.cs.webservice.domain.auths.UserAuth;
+import com.cs.webservice.domain.auths.UserInform;
+import com.cs.webservice.domain.auths.repository.EmailCertifyRepository;
+import org.joda.time.LocalDateTime;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class EmailRepositoryTest {
+    @Autowired
+    EmailCertifyRepository emailCertifyRepository;
+
+    @After
+    public void cleanup() {
+        emailCertifyRepository.deleteAll();
+    }
+
+    @Test
+    public void EmailCertifyAuthSaveAndLoad() {
+        LocalDateTime now = LocalDateTime.now();
+        EmailCertify emailCertify = EmailCertify.builder()
+                .email("jinhong0719@naver.com")
+                .authCode("a1b2c3")
+                .build();
+        emailCertifyRepository.save(emailCertify);
+
+        List<EmailCertify> emailCertifyList = emailCertifyRepository.findAll();
+
+        EmailCertify certify = emailCertifyList.get(0);
+
+        assertEquals(certify.getEmail(), "jinhong0719@naver.com");
+        assertEquals(certify.getAuthCode(), "a1b2c3");
+        assertFalse(certify.isCertified());
+        assertFalse(certify.isUsing());
+        assertTrue("error", certify.getCreatedAt().isAfter(now));
+        assertTrue("error", certify.getUpdatedAt().isAfter(now));
+    }
 }
