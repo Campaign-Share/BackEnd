29df5ad04dd5512a211ff9f6eb4f771bbc9dc1cc
지농 <jinhong0719@naver.com>
2020-10-31 17:00:38 +0900

    [ADD] <handler/auth/EmailHandler.java> Add verifyEmail method in interface

--- src/main/java/com/cs/webservice/handler/auth/EmailHandler.java
+++ src/main/java/com/cs/webservice/handler/auth/EmailHandler.java
@@ -1,6 +1,7 @@
 package com.cs.webservice.handler.auth;
 
 import com.cs.webservice.dto.auth.SendEmail;
+import com.cs.webservice.dto.auth.VerifyEmail;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -8,4 +9,5 @@ import javax.validation.Valid;
 
 public interface EmailHandler {
     SendEmail.Response sendEmail(@Valid @RequestBody SendEmail.Request req, BindingResult bindingResult);
+    VerifyEmail.Response verifyEmail(@Valid @RequestBody VerifyEmail.Request req, BindingResult bindingResult);
 }
