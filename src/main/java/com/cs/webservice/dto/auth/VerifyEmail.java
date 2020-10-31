d5b1a9b4b9c8eeca1700474e8bad6a4642469134
지농 <jinhong0719@naver.com>
2020-10-31 17:00:06 +0900

    [ADD] <dto/auths/VerifyEmail.java> Add Request & Response class of VerifyEmail

--- src/main/java/com/cs/webservice/dto/auth/VerifyEmail.java
+++ src/main/java/com/cs/webservice/dto/auth/VerifyEmail.java
@@ -1,2 +1,28 @@
-package com.cs.webservice.dto.auth;public class VerifyEmail {
+package com.cs.webservice.dto.auth;
+
+import com.cs.webservice.dto.BaseResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class VerifyEmail {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Request {
+        @NotEmpty @NotNull @Email
+        @JsonProperty("email")
+        private String email;
+        @NotEmpty @NotNull
+        @JsonProperty("auth_code")
+        private String authCode;
+    }
+
+    @NoArgsConstructor
+    public static class Response extends BaseResponse { }
 }
