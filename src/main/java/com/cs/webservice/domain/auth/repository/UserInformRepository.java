93208e3c25a2e3d6c7336d7309fa1577008bd903
지농 <jinhong0719@naver.com>
2020-10-31 18:07:37 +0900

    [ADD] <domain/.../UserInformRepository.java> Add findByEmail method in interface

--- src/main/java/com/cs/webservice/domain/auth/repository/UserInformRepository.java
+++ src/main/java/com/cs/webservice/domain/auth/repository/UserInformRepository.java
@@ -3,5 +3,8 @@ package com.cs.webservice.domain.auth.repository;
 import com.cs.webservice.domain.auth.UserInform;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserInformRepository extends JpaRepository<UserInform, Long> {
+    Optional<UserInform> findByEmail(String email);
 }
