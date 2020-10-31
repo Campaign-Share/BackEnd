7b45c6ed69ba10f511c694938b754e8d5ad3c115
지농 <jinhong0719@naver.com>
2020-10-31 12:39:19 +0900

    [ADD] <utils/Random.java> Add generateAlphanumericString static method

--- src/main/java/com/cs/webservice/utils/Random.java
+++ src/main/java/com/cs/webservice/utils/Random.java
@@ -1,2 +1,14 @@
-package com.cs.webservice.utils;public class Random {
+package com.cs.webservice.utils;
+
+public class Random {
+    public static String generateAlphanumericString(int targetStringLength) {
+        int leftLimit = 48;
+        int rightLimit = 122;
+        java.util.Random random = new java.util.Random();
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
 }
