83b9fc73ec1e29ea02892733c2107de9d8a568c1
지농 <jinhong0719@naver.com>
2020-10-31 16:12:34 +0900

    [ADD] <domain/auth/repository/EmailCertifyRepository.java> Add findByEmail

--- src/main/java/com/cs/webservice/domain/auth/repository/EmailCertifyRepository.java
+++ src/main/java/com/cs/webservice/domain/auth/repository/EmailCertifyRepository.java
@@ -3,5 +3,8 @@ package com.cs.webservice.domain.auth.repository;
 import com.cs.webservice.domain.auth.EmailCertify;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmailCertifyRepository extends JpaRepository<EmailCertify, String> {
+    Optional<EmailCertify> findByEmail(String email);
 }
