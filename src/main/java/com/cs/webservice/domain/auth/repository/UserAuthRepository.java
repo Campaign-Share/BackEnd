36d05d0c5799de1f2895233a5976a1f08ace38c9
지농 <jinhong0719@naver.com>
2020-10-31 18:07:10 +0900

    [ADD] <domain/.../UserAuthRepository.java> Add findBuUserID method in interface

--- src/main/java/com/cs/webservice/domain/auth/repository/UserAuthRepository.java
+++ src/main/java/com/cs/webservice/domain/auth/repository/UserAuthRepository.java
@@ -3,5 +3,8 @@ package com.cs.webservice.domain.auth.repository;
 import com.cs.webservice.domain.auth.UserAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserAuthRepository extends JpaRepository<UserAuth, String> {
+    Optional<UserAuth> findByUserID(String userID);
 }
