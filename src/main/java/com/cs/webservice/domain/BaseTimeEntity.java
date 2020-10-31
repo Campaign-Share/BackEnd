c4c4b3d38e3c477b0f1ee3c432addea7172aa275
지농 <jinhong0719@naver.com>
2020-10-31 16:11:04 +0900

    [MODIFY] <domain/BaseTimeEntity.java> Change LocalDateTime import package

--- src/main/java/com/cs/webservice/domain/BaseTimeEntity.java
+++ src/main/java/com/cs/webservice/domain/BaseTimeEntity.java
@@ -1,13 +1,13 @@
 package com.cs.webservice.domain;
 
 import lombok.Getter;
-import org.joda.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
