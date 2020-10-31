aba5b673aee674fc34aacf9d784ae56d5d17308e
지농 <jinhong0719@naver.com>
2020-10-31 13:04:38 +0900

    [MODIFY] <controller/auth/EmailController.java> Change how to handle request

--- src/main/java/com/cs/webservice/controller/auth/EmailController.java
+++ src/main/java/com/cs/webservice/controller/auth/EmailController.java
@@ -1,7 +1,8 @@
 package com.cs.webservice.controller.auth;
 
-import com.cs.webservice.domain.auths.repository.EmailCertifyRepository;
-import com.cs.webservice.dto.auths.SendEmail;
+import com.cs.webservice.domain.auth.repository.EmailCertifyRepository;
+import com.cs.webservice.dto.auth.SendEmail;
+import com.cs.webservice.handler.auth.EmailHandlerImpl;
 import com.cs.webservice.utils.Random;
 import lombok.AllArgsConstructor;
 import org.apache.http.HttpStatus;
@@ -15,22 +16,10 @@ import javax.validation.Valid;
 @RestController
 @AllArgsConstructor
 public class EmailController {
-    private EmailCertifyRepository emailCertifyRepository;
+    private final EmailHandlerImpl emailHandler;
 
     @PostMapping(value = "/v1/emails", consumes = {"application/json"})
     public SendEmail.Response sendEmail(@Valid @RequestBody SendEmail.Request req, BindingResult bindingResult) {
-        var resp = new SendEmail.Response();
-        if (bindingResult.hasErrors()) {
-            resp.setStatus(HttpStatus.SC_BAD_REQUEST);
-            resp.setMessage(bindingResult.getAllErrors().toString());
-            return resp;
-        }
-
-        String randomString = Random.generateAlphanumericString(8);
-
-
-        resp.setStatus(HttpStatus.SC_OK);
-        resp.setMessage("succeed to send email to certify");
-        return resp;
+        return emailHandler.sendEmail(req, bindingResult);
     }
 }
