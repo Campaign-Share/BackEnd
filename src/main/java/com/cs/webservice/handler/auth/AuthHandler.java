13af5038f57cfd7afb1affec43c77c6afd20f27c
지농 <jinhong0719@naver.com>
2020-10-31 12:48:26 +0900

    [ADD] <handler/auth/AuthHandler.java> Add createNewUser method in interface

--- src/main/java/com/cs/webservice/handler/auth/AuthHandler.java
+++ src/main/java/com/cs/webservice/handler/auth/AuthHandler.java
@@ -1,4 +1,11 @@
 package com.cs.webservice.handler.auth;
 
+import com.cs.webservice.dto.auth.CreateNewUser;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
 public interface AuthHandler {
+    CreateNewUser.Response createNewUser(@Valid @RequestBody CreateNewUser.Request dto, BindingResult bindingResult);
 }
