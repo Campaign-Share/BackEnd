329e98943c5748a4580a1172519f00666db31c91
지농 <jinhong0719@naver.com>
2020-10-31 18:08:30 +0900

    [ADD] <domain/.../UserAuthRepository.java> Add getAvailableUUID default method

--- src/main/java/com/cs/webservice/domain/auth/repository/UserAuthRepository.java
+++ src/main/java/com/cs/webservice/domain/auth/repository/UserAuthRepository.java
@@ -1,10 +1,20 @@
 package com.cs.webservice.domain.auth.repository;
 
 import com.cs.webservice.domain.auth.UserAuth;
+import com.cs.webservice.utils.Random;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserAuthRepository extends JpaRepository<UserAuth, String> {
     Optional<UserAuth> findByUserID(String userID);
+
+    default String getAvailableUUID() {
+        while (true) {
+            String userUUID = "user-" + Random.generateNumberString(12);
+            if (this.findById(userUUID).isEmpty()) {
+                return userUUID;
+            }
+        }
+    }
 }
