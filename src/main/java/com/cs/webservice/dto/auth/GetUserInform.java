304c8270346b5e7722a9517c831c1c6d2dca7b8d
지농 <jinhong0719@naver.com>
2020-11-01 15:55:37 +0900

    [ADD] <dto/auth/GetUserInform.java> Add Response static class in GerUserInform

--- src/main/java/com/cs/webservice/dto/auth/GetUserInform.java
+++ src/main/java/com/cs/webservice/dto/auth/GetUserInform.java
@@ -1,4 +1,21 @@
 package com.cs.webservice.dto.auth;
 
+import com.cs.webservice.dto.BaseResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 public class GetUserInform {
+    @Setter
+    @NoArgsConstructor
+    public static class Response extends BaseResponse {
+        @JsonProperty("user_uuid")
+        private String userUUID;
+        @JsonProperty("name")
+        private String name;
+        @JsonProperty("nick_name")
+        private String nickName;
+        @JsonProperty("email")
+        private String email;
+    }
 }
