aa918a7bd40c62cbad73696ed29e59029afda17c
지농 <jinhong0719@naver.com>
2020-11-01 15:17:12 +0900

    [ADD] <handler/auth/AuthHandler.java> Add changeUserPW method in interface

--- src/main/java/com/cs/webservice/handler/auth/AuthHandler.java
+++ src/main/java/com/cs/webservice/handler/auth/AuthHandler.java
@@ -1,13 +1,16 @@
 package com.cs.webservice.handler.auth;
 
+import com.cs.webservice.dto.auth.ChangeUserPW;
 import com.cs.webservice.dto.auth.CreateNewUser;
 import com.cs.webservice.dto.auth.LoginUserAuth;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import javax.validation.Valid;
 
 public interface AuthHandler {
     CreateNewUser.Response createNewUser(@Valid @RequestBody CreateNewUser.Request req, BindingResult bindingResult);
     LoginUserAuth.Response loginUserAuth(@Valid @RequestBody LoginUserAuth.Request req, BindingResult bindingResult);
+    ChangeUserPW.Response changeUserPW(@Valid @RequestBody @RequestHeader ChangeUserPW.Request req, BindingResult bindingResult, String token);
 }
