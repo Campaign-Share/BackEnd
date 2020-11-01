7067b7a0399ea9693f792747dec91a9acbda753e
지농 <jinhong0719@naver.com>
2020-11-01 15:41:32 +0900

    [ADD] <controller/auth/AuthController.java> Add prefix uri in AuthController

--- src/main/java/com/cs/webservice/controller/auth/AuthController.java
+++ src/main/java/com/cs/webservice/controller/auth/AuthController.java
@@ -1,29 +1,26 @@
 package com.cs.webservice.controller.auth;
 
-import com.cs.webservice.domain.auth.repository.UserAuthRepository;
 import com.cs.webservice.dto.auth.CreateNewUser;
 import com.cs.webservice.dto.auth.LoginUserAuth;
 import com.cs.webservice.handler.auth.AuthHandlerImpl;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.apache.http.HttpStatus;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/v1")
 public class AuthController {
     private final AuthHandlerImpl authHandler;
 
-    @PostMapping(value = "/v1/users", consumes = {"application/json"})
+    @PostMapping(path = "/users", consumes = {"application/json"})
     public CreateNewUser.Response createNewUser(@Valid @RequestBody CreateNewUser.Request req, BindingResult bindingResult) {
         return authHandler.createNewUser(req, bindingResult);
     }
-    @PostMapping(value = "/v1/login/user", consumes = {"application/json"})
+
+    @PostMapping(path = "/login/user", consumes = {"application/json"})
     public LoginUserAuth.Response loginUserAuth(@Valid @RequestBody LoginUserAuth.Request req, BindingResult bindingResult) {
         return authHandler.loginUserAuth(req, bindingResult);
     }
