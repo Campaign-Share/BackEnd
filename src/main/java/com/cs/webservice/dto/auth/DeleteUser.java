e4ba9dfc21b95f7d180ad509357d5fbc3821e0f4
지농 <jinhong0719@naver.com>
2020-11-01 21:53:18 +0900

    [ADD] <dto/auth/DeleteUser.java> Add Response static class of deleteUser

--- src/main/java/com/cs/webservice/dto/auth/DeleteUser.java
+++ src/main/java/com/cs/webservice/dto/auth/DeleteUser.java
@@ -1,2 +1,12 @@
-package com.cs.webservice.dto.auth;public class DeleteUser {
+package com.cs.webservice.dto.auth;
+
+import com.cs.webservice.dto.BaseResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class DeleteUser {
+    @Setter
+    @NoArgsConstructor
+    public static class Response extends BaseResponse { }
 }
