ac25ba4a13627abaaa1d7834ff331b985891d972
지농 <jinhong0719@naver.com>
2020-11-01 15:18:25 +0900

    [ADD] <utils/JwtTokenProvider.java> Add method about parsing jwt token

--- src/main/java/com/cs/webservice/utils/JwtTokenProvider.java
+++ src/main/java/com/cs/webservice/utils/JwtTokenProvider.java
@@ -1,14 +1,17 @@
 package com.cs.webservice.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.UnsupportedEncodingException;
 import java.security.Key;
 import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtTokenProvider {
@@ -18,7 +21,7 @@ public class JwtTokenProvider {
     @Value("${security.jwt.token.expire-length}")
     private long validityInMilliseconds;
 
-    public String createToken(String userUUID) {
+    public String generateAccessToken(String userUUID) {
         return Jwts.builder()
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + validityInMilliseconds * 1000))
@@ -26,5 +29,22 @@ public class JwtTokenProvider {
                 .claim("type", "access_token")
                 .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public boolean isSupportedAuthorization(String token) {
+        switch (token.split(" ")[0]) {
+            case "Bearer":
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    public Map<String, Object> getSubjectFrom(String jwt) {
+        return Jwts.parser()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .parseClaimsJws(jwt)
+                .getBody();
     }
 }
