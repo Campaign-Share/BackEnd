4057caa4f793ce379df91b3ec8c85017b332d250
지농 <jinhong0719@naver.com>
2020-11-01 22:54:59 +0900

    [ADD] <dto/auth/ChangeUserInform.java> Add Request & Response static class

--- src/main/java/com/cs/webservice/dto/auth/ChangeUserInform.java
+++ src/main/java/com/cs/webservice/dto/auth/ChangeUserInform.java
@@ -1,2 +1,35 @@
-package com.cs.webservice.dto.auth;public class ChangeUserInform {
+package com.cs.webservice.dto.auth;
+
+import com.cs.webservice.dto.BaseResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class ChangeUserInform {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Request {
+        @Size(min = 2, max = 5)
+        @JsonProperty("name")
+        private String name;
+
+        @Size(max = 10)
+        @JsonProperty("nick_name")
+        private String nickName;
+
+        @Size(max = 30) @Email
+        @JsonProperty("email")
+        private String email;
+    }
+
+    @Setter
+    @NoArgsConstructor
+    public static class Response extends BaseResponse { }
 }
