784c3ef610ceb6f96ec27028aabc1b4619c19cd4
지농 <jinhong0719@naver.com>
2020-11-01 13:20:24 +0900

    [ADD] <utils/JwtTokenProvider.java> Add private field & createToken method

--- src/main/java/com/cs/webservice/utils/JwtTokenProvider.java
+++ src/main/java/com/cs/webservice/utils/JwtTokenProvider.java
@@ -1,2 +1,30 @@
-package com.cs.webservice.utils;public class JwtTokenProvider {
+package com.cs.webservice.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.expire-length}")
+    private long validityInMilliseconds;
+
+    public String createToken(String userUUID) {
+        return Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + validityInMilliseconds * 1000))
+                .claim("uuid", userUUID)
+                .claim("type", "access_token")
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
