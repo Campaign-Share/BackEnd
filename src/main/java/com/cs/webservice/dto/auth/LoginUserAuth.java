01d4d2e83938c46ad2c48a431746cd09a05a23f3
지농 <jinhong0719@naver.com>
2020-11-01 11:40:02 +0900

    [ADD] <dto/auth/LoginUserAuth.java> Add Request & Response static class

--- src/main/java/com/cs/webservice/dto/auth/LoginUserAuth.java
+++ src/main/java/com/cs/webservice/dto/auth/LoginUserAuth.java
@@ -1,2 +1,34 @@
-package com.cs.webservice.dto.auth;public class LoginUserAuth {
+package com.cs.webservice.dto.auth;
+
+import com.cs.webservice.dto.BaseResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class LoginUserAuth {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Request {
+        @NotNull @NotEmpty
+        @JsonProperty("user_id")
+        private String userID;
+
+        @NotNull @NotEmpty
+        @JsonProperty("user_pw")
+        private String userPW;
+    }
+
+    @Setter
+    @NoArgsConstructor
+    public static class Response extends BaseResponse {
+        @JsonProperty("user_uuid")
+        private String userUUID;
+        @JsonProperty("access_token")
+        private String accessToken;
+    }
 }
