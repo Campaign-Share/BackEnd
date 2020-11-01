183c53988f72ae6151555c2594e757fad7e1b8c0
지농 <jinhong0719@naver.com>
2020-11-01 15:14:14 +0900

    [ADD] <handler/BaseHandler.java> Add AuthenticateResult static class

--- src/main/java/com/cs/webservice/handler/BaseHandler.java
+++ src/main/java/com/cs/webservice/handler/BaseHandler.java
@@ -1,4 +1,10 @@
 package com.cs.webservice.handler;
 
 public class BaseHandler {
+    public static class AuthenticateResult {
+        public boolean authorized;
+        public int code;
+        public String message;
+        public String uuid;
+    }
 }
