86a87a1306007aef62b454af3686e114c4c96d17
지농 <jinhong0719@naver.com>
2020-11-03 20:09:00 +0900

    [MODIFY] <handler/auth/AuthHandler.java> Delete @annotation in arguments

--- src/main/java/com/cs/webservice/handler/auth/AuthHandler.java
+++ src/main/java/com/cs/webservice/handler/auth/AuthHandler.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import javax.validation.Valid;
 
 public interface AuthHandler {
-    CreateNewUser.Response createNewUser(@Valid @RequestBody CreateNewUser.Request req, BindingResult bindingResult);
-    LoginUserAuth.Response loginUserAuth(@Valid @RequestBody LoginUserAuth.Request req, BindingResult bindingResult);
+    CreateNewUser.Response createNewUser(CreateNewUser.Request req, BindingResult bindingResult);
+    LoginUserAuth.Response loginUserAuth(LoginUserAuth.Request req, BindingResult bindingResult);
     ChangeUserPW.Response changeUserPW(ChangeUserPW.Request req, BindingResult bindingResult, String token, String userUUID);
     GetUserInform.Response getUserInform(String token, String userUUID);
     DeleteUser.Response deleteUser(String token, String userUUID);
