1cf946a745a467e21592ace27bcbbe83e65503a4
지농 <jinhong0719@naver.com>
2020-11-04 18:37:35 +0900

    [ADD] <domain/auth/UserAuth.java> Add @OneToMany to add fk in campaign

--- src/main/java/com/cs/webservice/domain/auth/UserAuth.java
+++ src/main/java/com/cs/webservice/domain/auth/UserAuth.java
@@ -1,23 +1,26 @@
 package com.cs.webservice.domain.auth;
 
 import com.cs.webservice.domain.BaseTimeEntity;
+import com.cs.webservice.domain.campaign.Campaign;
 import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import java.util.Set;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 @Setter
-@Entity
+@Entity(name = "user_auths")
 @Table(name = "user_auths")
 public class UserAuth extends BaseTimeEntity {
     @Id
     @Column(unique = true, nullable = false, columnDefinition = "CHAR(17)", length = 17, name = "uuid")
-    @Size(min = 17, max = 17) @NotNull @NotEmpty
+    @Size(min = 17, max = 17) @NotNull @NotEmpty @Pattern(regexp = "^user-\\d{12}")
     private String uuid;
 
     @Column(unique = true, nullable = false, length = 20, name = "user_id")
@@ -37,6 +40,9 @@ public class UserAuth extends BaseTimeEntity {
     @OneToOne(mappedBy = "userAuth", cascade = CascadeType.ALL)
     @PrimaryKeyJoinColumn
     private UserInform userInform;
+
+    @OneToMany(mappedBy = "userAuth", cascade = CascadeType.ALL)
+    private Set<Campaign> campaigns;
 
     @Builder
     public UserAuth(String uuid, String userId, String userPW) {
