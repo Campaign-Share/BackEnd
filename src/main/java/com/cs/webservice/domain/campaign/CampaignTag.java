b6524b72ee073420991969a3634514f0a5d26fe8
지농 <jinhong0719@naver.com>
2020-11-04 19:11:57 +0900

    [ADD] <domain/campaign/CampaignTag.java> Add field in CampaignTag as db table

--- src/main/java/com/cs/webservice/domain/campaign/CampaignTag.java
+++ src/main/java/com/cs/webservice/domain/campaign/CampaignTag.java
@@ -1,2 +1,32 @@
-package com.cs.webservice.domain.campaign;public class CampaignTag {
+package com.cs.webservice.domain.campaign;
+
+import com.cs.webservice.domain.BaseTimeEntity;
+import com.cs.webservice.domain.auth.AdminAuth;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "campaign_tags")
+@Table(name = "campaign_tags")
+public class CampaignTag extends BaseTimeEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, length = 20, name = "tag")
+    @Size(max = 10) @NotNull
+    private String tag;
+
+    @ManyToOne
+    @JoinColumn(name = "campaign_uuid",  referencedColumnName = "uuid", nullable = false)
+    @Size(min = 21, max = 21) @NotNull @NotEmpty @Pattern(regexp = "^campaign-\\d{12}")
+    private Campaign campaign;
 }
