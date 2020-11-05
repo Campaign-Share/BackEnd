2ddbba0946a5d0d47744b9b0101a147bcad1f71c
지농 <jinhong0719@naver.com>
2020-11-05 09:14:56 +0900

    [ADD] <.../CampaignController.java> Add mapping reqeust to createNewCampaign

--- src/main/java/com/cs/webservice/controller/campaign/CampaignController.java
+++ src/main/java/com/cs/webservice/controller/campaign/CampaignController.java
@@ -1,4 +1,23 @@
 package com.cs.webservice.controller.campaign;
 
+import com.cs.webservice.dto.campaign.CreateNewCampaign;
+import com.cs.webservice.handler.campaign.CampaignHandlerImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/v1")
 public class CampaignController {
+    private final CampaignHandlerImpl campaignHandler;
+
+    @PostMapping(path = "/campaigns", consumes = {"multipart/form-data"})
+    public CreateNewCampaign.Response createNewCampaign(@Valid @ModelAttribute CreateNewCampaign.Request req, BindingResult bindingResult,
+                                                        @RequestHeader(value = "Authorization", required = false) String token) throws IOException {
+        return campaignHandler.createNewCampaign(req, bindingResult, token);
+    }
 }
