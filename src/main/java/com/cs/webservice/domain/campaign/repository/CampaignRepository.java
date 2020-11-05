73b546fbdf0efa2a45d4bba7c0d49b743db27ba1
지농 <jinhong0719@naver.com>
2020-11-05 09:09:03 +0900

    [ADD] <domain/.../CampaignRepository.java> Add getAvailableUUID default method

--- src/main/java/com/cs/webservice/domain/campaign/repository/CampaignRepository.java
+++ src/main/java/com/cs/webservice/domain/campaign/repository/CampaignRepository.java
@@ -1,6 +1,8 @@
 package com.cs.webservice.domain.campaign.repository;
 
+import com.cs.webservice.domain.auth.UserAuth;
 import com.cs.webservice.domain.campaign.Campaign;
+import com.cs.webservice.utils.Random;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -8,4 +10,13 @@ import java.util.List;
 
 public interface CampaignRepository extends JpaRepository<Campaign, String> {
     List<Campaign> findAllByUserUUIDAndEndDateGreaterThanEqual(String userUUID, LocalDate date);
+
+    default String getAvailableUUID() {
+        while (true) {
+            String campaignUUID = "campaign-" + Random.generateNumberString(12);
+            if (this.findById(campaignUUID).isEmpty()) {
+                return campaignUUID;
+            }
+        }
+    }
 }
