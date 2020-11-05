9c49ed8d2b5cbe666c2dc17e3e708078e049ff8d
지농 <jinhong0719@naver.com>
2020-11-05 09:09:41 +0900

    [ADD] <handler/campaign/CampaignHandler.java> Add createNewCampaign method

--- src/main/java/com/cs/webservice/handler/campaign/CampaignHandler.java
+++ src/main/java/com/cs/webservice/handler/campaign/CampaignHandler.java
@@ -1,2 +1,10 @@
-package com.cs.webservice.handler.campaign;public interface CampaignHandler {
+package com.cs.webservice.handler.campaign;
+
+import com.cs.webservice.dto.campaign.CreateNewCampaign;
+import org.springframework.validation.BindingResult;
+
+import java.io.IOException;
+
+public interface CampaignHandler {
+    CreateNewCampaign.Response createNewCampaign(CreateNewCampaign.Request req, BindingResult bindingResult, String token) throws IOException;
 }
