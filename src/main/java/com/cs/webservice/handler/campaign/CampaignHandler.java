e320d5dfb337ccca5800a69bcbb7a526f78e7f1d
지농 <jinhong0719@naver.com>
2020-11-05 13:01:49 +0900

    [ADD] <handler/campaign/CampaignHandler.java> Add getCampaignsWithUserUUID

--- src/main/java/com/cs/webservice/handler/campaign/CampaignHandler.java
+++ src/main/java/com/cs/webservice/handler/campaign/CampaignHandler.java
@@ -1,10 +1,12 @@
 package com.cs.webservice.handler.campaign;
 
 import com.cs.webservice.dto.campaign.CreateNewCampaign;
+import com.cs.webservice.dto.campaign.GetCampaignsWithUserUUID;
 import org.springframework.validation.BindingResult;
 
 import java.io.IOException;
 
 public interface CampaignHandler {
     CreateNewCampaign.Response createNewCampaign(CreateNewCampaign.Request req, BindingResult bindingResult, String token) throws IOException;
+    GetCampaignsWithUserUUID.Response getCampaignsWithUserUUID(String token, String userUUID);
 }
