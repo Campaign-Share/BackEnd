fe0b4ce1153161297e1c3c4d2264f6391f202632
지농 <jinhong0719@naver.com>
2020-11-05 12:56:53 +0900

    [ADD] <domain/.../CampaignTagRepository.java> Add findAllByCampaignUUID method

--- src/main/java/com/cs/webservice/domain/campaign/repository/CampaignTagRepository.java
+++ src/main/java/com/cs/webservice/domain/campaign/repository/CampaignTagRepository.java
@@ -3,5 +3,8 @@ package com.cs.webservice.domain.campaign.repository;
 import com.cs.webservice.domain.campaign.CampaignTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CampaignTagRepository extends JpaRepository<CampaignTag, Long> {
+    List<CampaignTag> findAllByCampaignUUID(String campaignUUID);
 }
