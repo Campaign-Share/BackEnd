7731c058d9c45cd9b0f6e219db721b16c5eeb808
지농 <jinhong0719@naver.com>
2020-11-05 09:07:06 +0900

    [ADD] <.../CampaignRepository.java> Add findAllByUserUUIDAndEndDateGreaterThanEqual

--- src/main/java/com/cs/webservice/domain/campaign/repository/CampaignRepository.java
+++ src/main/java/com/cs/webservice/domain/campaign/repository/CampaignRepository.java
@@ -3,5 +3,9 @@ package com.cs.webservice.domain.campaign.repository;
 import com.cs.webservice.domain.campaign.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface CampaignRepository extends JpaRepository<Campaign, String> {
+    List<Campaign> findAllByUserUUIDAndEndDateGreaterThanEqual(String userUUID, LocalDate date);
 }
