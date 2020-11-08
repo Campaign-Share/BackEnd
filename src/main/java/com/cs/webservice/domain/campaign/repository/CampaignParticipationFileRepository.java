6140160ba66cdcb1f48b0492b9ea76012ab0a716
지농 <jinhong0719@naver.com>
2020-11-08 22:55:32 +0900

    [CREATE <domain/campaign/repository/CampaignParticipationFileRepository.java>

---
+++ src/main/java/com/cs/webservice/domain/campaign/repository/CampaignParticipationFileRepository.java
@@ -0,0 +1,7 @@
+package com.cs.webservice.domain.campaign.repository;
+
+import com.cs.webservice.domain.campaign.CampaignParticipationFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampaignParticipationFileRepository extends JpaRepository<CampaignParticipationFile, Long> {
+}
