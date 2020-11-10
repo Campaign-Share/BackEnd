b5062116fc0de3e00a466948c06fd106a7e7be80
지농 <jinhong0719@naver.com>
2020-11-10 21:16:49 +0900

    [ADD] <dto/campaign/GetCampaignsSortedByCreate.java> Add Response static class

--- src/main/java/com/cs/webservice/dto/campaign/GetCampaignsSortedByCreate.java
+++ src/main/java/com/cs/webservice/dto/campaign/GetCampaignsSortedByCreate.java
@@ -1,2 +1,11 @@
-package com.cs.webservice.dto.campaign;public class GetCampaignsSortedByCreate {
+package com.cs.webservice.dto.campaign;
+
+import com.cs.webservice.dto.BaseResponse;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class GetCampaignsSortedByCreate {
+    @Setter
+    @NoArgsConstructor
+    public static class Response extends BaseResponse { }
 }
