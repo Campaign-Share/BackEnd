c29f5e27a4ff2aa4a268eaf288e220e8bb7adfe9
지농 <jinhong0719@naver.com>
2020-11-11 18:34:42 +0900

    [MODIFY] <utils/CampaignStatus.java> Change type of campaign status to int

--- src/main/java/com/cs/webservice/utils/CampaignStatus.java
+++ src/main/java/com/cs/webservice/utils/CampaignStatus.java
@@ -1,7 +1,7 @@
 package com.cs.webservice.utils;
 
 public class CampaignStatus {
-    public final static Integer PENDING = 0;
-    public final static Integer APPROVED = 1;
-    public final static Integer REJECTED = 2;
+    public final static int PENDING = 0;
+    public final static int APPROVED = 1;
+    public final static int REJECTED = 2;
 }
