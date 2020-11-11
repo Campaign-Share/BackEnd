90911284e0e55b578efcec500b798e46e6fbb13e
지농 <jinhong0719@naver.com>
2020-11-11 23:00:36 +0900

    [ADD] <handler/campaign/CampaignHandler.java> Add getCampaignsSortedByFamous

--- src/main/java/com/cs/webservice/handler/campaign/CampaignHandler.java
+++ src/main/java/com/cs/webservice/handler/campaign/CampaignHandler.java
@@ -2,6 +2,7 @@ package com.cs.webservice.handler.campaign;
 
 import com.cs.webservice.dto.campaign.CreateNewCampaign;
 import com.cs.webservice.dto.campaign.GetCampaignsSortedByCreate;
+import com.cs.webservice.dto.campaign.GetCampaignsSortedByFamous;
 import com.cs.webservice.dto.campaign.GetCampaignsWithUserUUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -12,5 +13,7 @@ public interface CampaignHandler {
     ResponseEntity<CreateNewCampaign.Response> createNewCampaign(CreateNewCampaign.Request req, BindingResult bindingResult, String token) throws IOException;
     ResponseEntity<GetCampaignsWithUserUUID.Response> getCampaignsWithUserUUID(String token, String userUUID);
     ResponseEntity<GetCampaignsSortedByCreate.Response> getCampaignsSortedByCreate(String token, Integer startPaging, Integer countPaging,
+                                                                                   String statusFilter, String tagFilter);
+    ResponseEntity<GetCampaignsSortedByFamous.Response> getCampaignsSortedByFamous(String token, Integer startPaging, Integer countPaging,
                                                                                    String statusFilter, String tagFilter);
 }
