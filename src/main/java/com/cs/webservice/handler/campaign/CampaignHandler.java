132f38206f584f5e64c2d308975cad1fba036051
지농 <jinhong0719@naver.com>
2020-11-11 10:16:42 +0900

    [ADD] <handler/campaign/CampaignHandler.java> Add getCampaignsSortedByCreate

--- src/main/java/com/cs/webservice/handler/campaign/CampaignHandler.java
+++ src/main/java/com/cs/webservice/handler/campaign/CampaignHandler.java
@@ -11,4 +11,6 @@ import java.io.IOException;
 public interface CampaignHandler {
     ResponseEntity<CreateNewCampaign.Response> createNewCampaign(CreateNewCampaign.Request req, BindingResult bindingResult, String token) throws IOException;
     ResponseEntity<GetCampaignsWithUserUUID.Response> getCampaignsWithUserUUID(String token, String userUUID);
+    ResponseEntity<GetCampaignsSortedByCreate.Response> getCampaignsSortedByCreate(String token, Integer startPaging, Integer countPaging,
+                                                                                   String statusFilter, String tagFilter);
 }
