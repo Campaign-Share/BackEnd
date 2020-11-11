1951d0a22648a9311a873833587fe090b38a4c60
지농 <jinhong0719@naver.com>
2020-11-11 16:55:52 +0900

    [CREATE] <utils/CampaignStatus.java> Create to declare static campaign status

---
+++ src/main/java/com/cs/webservice/utils/CampaignStatus.java
@@ -0,0 +1,7 @@
+package com.cs.webservice.utils;
+
+public class CampaignStatus {
+    public final static Integer PENDING = 0;
+    public final static Integer APPROVED = 1;
+    public final static Integer REJECTED = 2;
+}
