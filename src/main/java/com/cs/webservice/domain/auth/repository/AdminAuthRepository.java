b3bb65f41ae5538f00277bea9f467224033544f5
지농 <jinhong0719@naver.com>
2020-11-12 14:32:19 +0900

    [ADD] <domain/auth/.../AdminAuthRepository.java> Add findByAdminID method

--- src/main/java/com/cs/webservice/domain/auth/repository/AdminAuthRepository.java
+++ src/main/java/com/cs/webservice/domain/auth/repository/AdminAuthRepository.java
@@ -1,7 +1,11 @@
 package com.cs.webservice.domain.auth.repository;
 
 import com.cs.webservice.domain.auth.AdminAuth;
+import com.cs.webservice.domain.auth.UserAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminAuthRepository extends JpaRepository<AdminAuth, String> {
+    Optional<AdminAuth> findByAdminID(String adminID);
 }
