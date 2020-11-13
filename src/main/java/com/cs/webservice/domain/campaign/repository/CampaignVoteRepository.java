73850bb80b0f396198aebd281ee1795c28c147a0
지농 <jinhong0719@naver.com>
2020-11-13 14:27:53 +0900

    [ADD] <domain/campaign/.../CampaignVoteRepository.java> Add read & delete method

--- src/main/java/com/cs/webservice/domain/campaign/repository/CampaignVoteRepository.java
+++ src/main/java/com/cs/webservice/domain/campaign/repository/CampaignVoteRepository.java
@@ -3,5 +3,12 @@ package com.cs.webservice.domain.campaign.repository;
 import com.cs.webservice.domain.campaign.CampaignVote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
+
 public interface CampaignVoteRepository extends JpaRepository<CampaignVote, Long> {
+    Optional<CampaignVote> findByVoterUUIDAndCampaignUUID(String voterUUID, String campaignUUID);
+    Optional<CampaignVote> findByVoterUUIDAndCampaignUUIDAndAgree(String voterUUID, String campaignUUID, boolean agree);
+    @Transactional
+    void deleteByVoterUUIDAndCampaignUUID(String voterUUID, String campaignUUID);
 }
