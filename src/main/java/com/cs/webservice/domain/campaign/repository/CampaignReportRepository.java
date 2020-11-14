931e205b685894933ef60e73124705f296830f98
지농 <jinhong0719@naver.com>
2020-11-14 13:20:53 +0900

    [ADD] <domain/.../CampaignReportRepository.java> Add findByReporterUUIDAndTargetUUID

--- src/main/java/com/cs/webservice/domain/campaign/repository/CampaignReportRepository.java
+++ src/main/java/com/cs/webservice/domain/campaign/repository/CampaignReportRepository.java
@@ -1,7 +1,21 @@
 package com.cs.webservice.domain.campaign.repository;
 
 import com.cs.webservice.domain.campaign.CampaignReport;
+import com.cs.webservice.domain.campaign.CampaignVote;
+import com.cs.webservice.utils.Random;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CampaignReportRepository extends JpaRepository<CampaignReport, String> {
+    Optional<CampaignReport> findByReporterUUIDAndTargetUUID(String reporterUUID, String targetUUID);
+
+    default String getAvailableUUID() {
+        while (true) {
+            String reportUUID = "report-" + Random.generateNumberString(12);
+            if (this.findById(reportUUID).isEmpty()) {
+                return reportUUID;
+            }
+        }
+    }
 }
