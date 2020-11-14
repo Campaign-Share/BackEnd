4b6f56cb3967ef76f843b8c3cdb9f749eded4a54
지농 <jinhong0719@naver.com>
2020-11-14 19:06:25 +0900

    [ADD] <domain/campaign/.../CampaignRepository.java> Add sanctioned statement

--- src/main/java/com/cs/webservice/domain/campaign/repository/CampaignRepository.java
+++ src/main/java/com/cs/webservice/domain/campaign/repository/CampaignRepository.java
@@ -16,42 +16,42 @@ public interface CampaignRepository extends JpaRepository<Campaign, String> {
     Optional<Campaign> findByUuid(String campaignUUID);
 
     @Query(value = "SELECT * FROM campaigns WHERE uuid IN (SELECT DISTINCT campaign_uuid FROM campaign_tags WHERE tag=?1) " +
-            "AND status=?2 AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY created_at DESC LIMIT ?4 OFFSET ?3", nativeQuery = true)
+            "AND status=?2 AND sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY created_at DESC LIMIT ?4 OFFSET ?3", nativeQuery = true)
     List<Campaign> findAllByTagAndStatusWithPagingSortedByCreatedAt(String tag, Integer status, Integer start, Integer count);
-    @Query(value = "SELECT * FROM campaigns WHERE status=?1 AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY created_at DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM campaigns WHERE status=?1 AND sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY created_at DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
     List<Campaign> findAllByStatusWithPagingSortedByCreatedAt(Integer status, Integer start, Integer count);
     @Query(value = "SELECT * FROM campaigns WHERE uuid IN (SELECT DISTINCT campaign_uuid FROM campaign_tags WHERE tag=?1) " +
-            "AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY created_at DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
+            "AND sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY created_at DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
     List<Campaign> findAllByTagWithPagingSortedByCreatedAt(String tag, Integer start, Integer count);
-    @Query(value = "SELECT * FROM campaigns WHERE CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY created_at DESC LIMIT ?2 OFFSET ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM campaigns WHERE sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY created_at DESC LIMIT ?2 OFFSET ?1", nativeQuery = true)
     List<Campaign> findAllWithPagingSortedByCreatedAt(Integer start, Integer count);
 
 
     @Query(value = "SELECT * FROM campaigns WHERE uuid IN (SELECT DISTINCT campaign_uuid FROM campaign_tags WHERE tag=?1) " +
-            "AND status=?2 AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY agree_number + disagree_number DESC LIMIT ?4 OFFSET ?3", nativeQuery = true)
+            "AND status=?2 AND sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY agree_number + disagree_number DESC LIMIT ?4 OFFSET ?3", nativeQuery = true)
     List<Campaign> findAllByTagAndStatusWithPagingSortedByFamous(String tag, Integer status, Integer start, Integer count);
-    @Query(value = "SELECT * FROM campaigns WHERE status=?1 AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY agree_number + disagree_number DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM campaigns WHERE status=?1 AND sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY agree_number + disagree_number DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
     List<Campaign> findAllByStatusWithPagingSortedByFamous(Integer status, Integer start, Integer count);
     @Query(value = "SELECT * FROM campaigns WHERE uuid IN (SELECT DISTINCT campaign_uuid FROM campaign_tags WHERE tag=?1) " +
-            "AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY agree_number + disagree_number DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
+            "AND sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY agree_number + disagree_number DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
     List<Campaign> findAllByTagWithPagingSortedByFamous(String tag, Integer start, Integer count);
-    @Query(value = "SELECT * FROM campaigns WHERE CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY agree_number + disagree_number DESC LIMIT ?2 OFFSET ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM campaigns WHERE sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY agree_number + disagree_number DESC LIMIT ?2 OFFSET ?1", nativeQuery = true)
     List<Campaign> findAllWithPagingSortedByFamous(Integer start, Integer count);
 
     @Query(value = "SELECT * FROM campaigns WHERE uuid IN (SELECT DISTINCT campaign_uuid FROM campaign_tags WHERE tag=?1) " +
-            "AND status=?2 AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY RAND() DESC LIMIT ?4 OFFSET ?3", nativeQuery = true)
+            "AND status=?2 AND sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY RAND() DESC LIMIT ?4 OFFSET ?3", nativeQuery = true)
     List<Campaign> findAllByTagAndStatusWithPagingSortedByRandom(String tag, Integer status, Integer start, Integer count);
-    @Query(value = "SELECT * FROM campaigns WHERE status=?1 AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY RAND() DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM campaigns WHERE status=?1 AND sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY RAND() DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
     List<Campaign> findAllByStatusWithPagingSortedByRandom(Integer status, Integer start, Integer count);
     @Query(value = "SELECT * FROM campaigns WHERE uuid IN (SELECT DISTINCT campaign_uuid FROM campaign_tags WHERE tag=?1) " +
-            "AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY RAND() DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
+            "AND sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY RAND() DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
     List<Campaign> findAllByTagWithPagingSortedByRandom(String tag, Integer start, Integer count);
-    @Query(value = "SELECT * FROM campaigns WHERE CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY RAND() DESC LIMIT ?2 OFFSET ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM campaigns WHERE sanctioned=false AND CURRENT_DATE() <= CAST(end_date AS DATETIME) ORDER BY RAND() DESC LIMIT ?2 OFFSET ?1", nativeQuery = true)
     List<Campaign> findAllWithPagingSortedByRandom(Integer start, Integer count);
 
-    @Query(value = "SELECT * FROM campaigns WHERE user_uuid = ?1 ORDER BY created_at DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM campaigns WHERE user_uuid = ?1 AND sanctioned=false ORDER BY created_at DESC LIMIT ?3 OFFSET ?2", nativeQuery = true)
     List<Campaign> findAllByUserUUIDWithPagingSortedByCreatedAt(String userUUID, Integer start, Integer count);
-    @Query(value = "SELECT * FROM campaigns WHERE user_uuid = ?1 AND status=?2 ORDER BY created_at DESC LIMIT ?4 OFFSET ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM campaigns WHERE user_uuid = ?1 AND status=?2 AND sanctioned=false ORDER BY created_at DESC LIMIT ?4 OFFSET ?3", nativeQuery = true)
     List<Campaign> findAllByUserUUIDAndStatusWithPagingSortedByCreatedAt(String userUUID, Integer status, Integer start, Integer count);
 
     default String getAvailableUUID() {
