912d28bf9cfa5267426c8b44cd631430f5c5dc52
지농 <jinhong0719@naver.com>
2020-11-15 00:07:37 +0900

    [ADD] <domain/.../UserInformRepository.java> Add findAllWithPagingSortedByParticipate

--- src/main/java/com/cs/webservice/domain/auth/repository/UserInformRepository.java
+++ src/main/java/com/cs/webservice/domain/auth/repository/UserInformRepository.java
@@ -3,10 +3,15 @@ package com.cs.webservice.domain.auth.repository;
 import com.cs.webservice.domain.auth.UserAuth;
 import com.cs.webservice.domain.auth.UserInform;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserInformRepository extends JpaRepository<UserInform, Long> {
     Optional<UserInform> findByEmail(String email);
     Optional<UserInform> findByUserAuth(UserAuth userAuth);
+
+    @Query(value = "SELECT * FROM user_informs ORDER BY participation_number DESC LIMIT ?2 OFFSET ?1", nativeQuery = true)
+    List<UserInform> findAllWithPagingSortedByParticipationNumber(Integer start, Integer count);
 }
