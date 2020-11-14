652d5be55c3b7e42eb9911958c340ec25ec4023a
지농 <jinhong0719@naver.com>
2020-11-14 14:27:19 +0900

    [ADD] <domain/campaign/.../CampaignReportRepository.java> Add get reports method

--- src/main/java/com/cs/webservice/domain/campaign/repository/CampaignReportRepository.java
+++ src/main/java/com/cs/webservice/domain/campaign/repository/CampaignReportRepository.java
@@ -4,11 +4,16 @@ import com.cs.webservice.domain.campaign.CampaignReport;
 import com.cs.webservice.domain.campaign.CampaignVote;
 import com.cs.webservice.utils.Random;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CampaignReportRepository extends JpaRepository<CampaignReport, String> {
     Optional<CampaignReport> findByReporterUUIDAndTargetUUID(String reporterUUID, String targetUUID);
+
+    @Query(value = "SELECT * FROM campaign_reports WHERE handled = ?1 AND sanctioned = ?2 ORDER BY created_at DESC LIMIT ?4 OFFSET ?3", nativeQuery = true)
+    List<CampaignReport> findAllByHandledAndSanctionedWithPagingSortedByCreatedAt(boolean handled, boolean sanctioned, Integer start, Integer count);
 
     default String getAvailableUUID() {
         while (true) {
