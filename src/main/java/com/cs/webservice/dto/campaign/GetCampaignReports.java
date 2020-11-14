0859648ed7b3af2a16ffee2bf4cbd2affb4c717b
지농 <jinhong0719@naver.com>
2020-11-14 14:25:58 +0900

    [CREATE] <dto/campaign/GetCampaignReports.java> Create to declare GetCampaignReports

---
+++ src/main/java/com/cs/webservice/dto/campaign/GetCampaignReports.java
@@ -0,0 +1,17 @@
+package com.cs.webservice.dto.campaign;
+
+import com.cs.webservice.dto.BaseResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+public class GetCampaignReports {
+    @Setter
+    @NoArgsConstructor
+    public static class Response extends BaseResponse {
+        @JsonProperty("campaign_reports")
+        List<CampaignReportDTO> campaignReports;
+    }
+}
