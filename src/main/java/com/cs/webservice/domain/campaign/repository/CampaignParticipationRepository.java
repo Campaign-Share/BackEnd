160998dcecf663721908594b2d0c6e9c5866828e
지농 <jinhong0719@naver.com>
2020-11-16 13:59:57 +0900

    [ADD] <domain/campaign/.../CampaignParticipationRepository.java> Add find one method

--- src/main/java/com/cs/webservice/domain/campaign/repository/CampaignParticipationRepository.java
+++ src/main/java/com/cs/webservice/domain/campaign/repository/CampaignParticipationRepository.java
@@ -1,7 +1,20 @@
 package com.cs.webservice.domain.campaign.repository;
 
 import com.cs.webservice.domain.campaign.CampaignParticipation;
+import com.cs.webservice.utils.Random;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CampaignParticipationRepository extends JpaRepository<CampaignParticipation, String> {
+    Optional<CampaignParticipation> findByParticipantUUIDAndCampaignUUID(String participantUUID, String campaignUUID);
+
+    default String getAvailableUUID() {
+        while (true) {
+            String participationUUID = "participation-" + Random.generateNumberString(12);
+            if (this.findById(participationUUID).isEmpty()) {
+                return participationUUID;
+            }
+        }
+    }
 }
