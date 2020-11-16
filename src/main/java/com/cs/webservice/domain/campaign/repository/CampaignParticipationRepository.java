bcbf01b14c10a8b5eb0c6682c6e261f95e719aa3
지농 <jinhong0719@naver.com>
2020-11-16 16:46:13 +0900

    [ADD] <domain/.../CampaignParticipationRepository.java> Add method to get uuid list

--- src/main/java/com/cs/webservice/domain/campaign/repository/CampaignParticipationRepository.java
+++ src/main/java/com/cs/webservice/domain/campaign/repository/CampaignParticipationRepository.java
@@ -1,13 +1,19 @@
 package com.cs.webservice.domain.campaign.repository;
 
+import com.cs.webservice.domain.campaign.Campaign;
 import com.cs.webservice.domain.campaign.CampaignParticipation;
 import com.cs.webservice.utils.Random;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CampaignParticipationRepository extends JpaRepository<CampaignParticipation, String> {
     Optional<CampaignParticipation> findByParticipantUUIDAndCampaignUUID(String participantUUID, String campaignUUID);
+
+    @Query(value = "SELECT campaign_uuid FROM campaign_participations WHERE state=0 GROUP BY campaign_uuid ORDER BY COUNT(*) DESC LIMIT ?2 OFFSET ?1", nativeQuery = true)
+    List<String> findAllWithPagingSortedByTotalPendingNumber(Integer start, Integer count);
 
     default String getAvailableUUID() {
         while (true) {
