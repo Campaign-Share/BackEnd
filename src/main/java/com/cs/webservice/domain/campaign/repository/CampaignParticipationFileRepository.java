18ec93a4fc4286d94925871f548f029b2baaa8d2
지농 <jinhong0719@naver.com>
2020-11-16 13:17:04 +0900

    [REMOVE] <domain/campaign/repository/CampaignParticipationFIle.java> Delete entry

--- src/main/java/com/cs/webservice/domain/campaign/repository/CampaignParticipationFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cs.webservice.domain.campaign.repository;
-
-import com.cs.webservice.domain.campaign.CampaignParticipationFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampaignParticipationFileRepository extends JpaRepository<CampaignParticipationFile, Long> {
-}
